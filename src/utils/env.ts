68df24645c00ca5772b851bc978ee4c52ca98912
chenym1992 <18251821296@163.com>
2023-08-21 15:58:02 +0800

    Initial commit

---
+++ src/utils/env.ts
@@ -0,0 +1,16 @@
+export function isDevelopmentEnv(): boolean {
+  return process.env.NODE_ENV === "development";
+}
+
+export function isTestEnv(): boolean {
+  return process.env.NODE_ENV === "test";
+}
+
+export function isStagingEnv(): boolean {
+  // This is set in vercel builds and deploys from releases/staging.
+  return Boolean(process.env.REACT_APP_STAGING);
+}
+
+export function isProductionEnv(): boolean {
+  return process.env.NODE_ENV === "production" && !isStagingEnv();
+}
